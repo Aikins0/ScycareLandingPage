096f3691bf3598f2649dec8c9c8a83ced248e8e0
Daniel Aikins <aikinsdaniel01@gmail.com>
2024-02-26 18:36:50 +0000

    changes

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-function toggleQuestion(button) {
-    const answer = button.nextElementSibling;
-    if (answer.style.display === "block") {
-      answer.style.display = "none";
+const accordionBtns = document.querySelectorAll('.accordion-btn');
+
+accordionBtns.forEach((accordionBtn) => {
+  accordionBtn.addEventListener('click', () => {
+    const accordionContent = accordionBtn.nextElementSibling;
+    const plusSign = accordionBtn.querySelector('.plus-sign');
+
+    accordionBtns.forEach((otherAccordionBtn) => {
+      if (otherAccordionBtn !== accordionBtn) {
+        otherAccordionBtn.classList.remove('active');
+        otherAccordionBtn.nextElementSibling.style.maxHeight = null;
+        otherAccordionBtn.querySelector('.plus-sign').textContent = '+';
+      }
+    });
+
+    accordionBtn.classList.toggle('active');
+
+    if (accordionBtn.classList.contains('active')) {
+      accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      plusSign.textContent = '-';
     } else {
-      answer.style.display = "block";
+      accordionContent.style.maxHeight = null;
+      plusSign.textContent = '+';
     }
-  }
+  });
+});
